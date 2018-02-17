1d6993c8709d06348982fd8219e634e942cdf02b
sant <SANTU954@GMAIL.COM>
2018-02-18 03:51:38 +0530

    Added tooltip to tabledata in DataTables and much more

--- PED/static/g_chart.js
+++ PED/static/g_chart.js
@@ -1,194 +1,217 @@
 
+function draw_pie(data, e, title) {
+	Highcharts
+			.chart(
+					e,
+					{
+						chart : {
+							plotBackgroundColor : '#E3FFE3',
+							plotBorderWidth : null,
+							plotShadow : false,
+							type : 'pie'
+						},
+						title : {
+							text : title
+						},
+						credits : {
+							enabled : false
+						},
+						exporting : {
+							enabled : false
+						},
 
-function draw_pie(data,e){
-Highcharts.chart(e, {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    
-    
+						tooltip : {
+							pointFormat : '{series.name}: <b>{point.percentage:.1f}%</b>'
+						},
+						plotOptions : {
+							pie : {
+								allowPointSelect : true,
+								size : 200,
 
-    //var object={'positive':94,'neutral':2,'negative':2};
+								cursor : 'pointer',
+								dataLabels : {
+									enabled : true,
+									format : '<b>{point.name}</b>: {point.percentage:.1f} %',
+									style : {
+										color : (Highcharts.theme && Highcharts.theme.contrastTextColor)
+												|| 'black'
+									}
+								}
+							}
+						},
 
-	 
-    series: [{
-            name: 'Brands',
-            data: data
-    }]
-    
-    })};
-    
-    
+						series : [ {
+							name : 'Tickets',
+							colorByPoint : true,
+							data : data
+						} ]
 
-    function parseISOLocal(s) {
-    	  var b = s.split('-');
-    	  //datestring=String(parseInt(b[0])+1) + b[1] +b[2]
-    	  datestring=b[0] + b[1] +b[2]
-    	  return new Date(datestring);
-    	}
+					})
+};
 
+function parseISOLocal(s) {
+	var b = s.split('-');
+	// datestring=String(parseInt(b[0])+1) + b[1] +b[2]
+	datestring = b[0] + b[1] + b[2]
+	return new Date(datestring);
+}
 
-    
-    
-    function Incident_VS_SR(line,title,e){
-    	var  incident_data=[];
-		for(var i in line){
-			
-			incident_date=parseISOLocal(line[i][0])
-				incident_data.push([Date.parse(line[i][0]),line[i][1]]);
-			
-			
-		//	dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
-		}
-		var sr_data=[]
-for(var i in line){
-			
-			sr_date=parseISOLocal(line[i][0])
-				sr_data.push([Date.parse(line[i][0]),line[i][2]]);
-			
-			
-		//	dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
-		}
-    	
-    	
-    Highcharts.chart(e, {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-         },
-        
-         chart: {
-             zoomType: 'x'
-         },
-         title: {
-             text:  title
-         },
-         subtitle: {
-             text: document.ontouchstart === undefined ?
-                     'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-         },
-         xAxis: {
-        	 type: 'datetime',
-        	    labels: {
-        	      format: '{value:%Y-%b-%e}'
-        	    }},
-         yAxis: {
-             title: {
-                 text: 'Inflow Count'
-             }
-         },
-         legend: {
-             enabled: false
-         }, 
-        //var object={'positive':94,'neutral':2,'negative':2};
+function Incident_VS_SR(line, title, e) {
+	var incident_data = [];
+	for ( var i in line) {
 
-    	 
-        series: [{
-                name: "Incident",
-                data: incident_data
-        }
-           ,{
-                name: "SR",
-                data: sr_data
-        }]
-        
-        })};
-        
-        
-        
-        
-        function InflowVSOutflow(ticket_inflow,ticket_outflow,title,category,e){
-        	
-        	if (category=='Incident'){
-        		category_index	=1
-        	}else{
-        		category_index=2
-        	}
-        	/*var  inflow_data=[];
-    		for(var i in ticket_inflow){
-    			
-    			inflow_date=parseISOLocal(ticket_inflow[i][0])
-    				inflow_data.push({"name": inflow_date,"y":ticket_inflow[i][category_index]});
-    			
-    			
-    		//	dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
-    		}
-    		
-    		
-    		
-    		
-    		
-*/    		var  inflow_data=[];
-for(var i in ticket_inflow){
-    			 
-    				inflow_data.push([Date.parse(ticket_inflow[i][0]),ticket_inflow[i][category_index]]);
-    			
-    			
-    		//	dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
-    		}
-        	
-        	
-        	
-        	
-        	
-        	
-        	
-        	var outflow_data=[]
-    for(var i in ticket_outflow){
-    			
-    			//outflow_date=parseISOLocal(ticket_outflow[i][0])
-    				outflow_data.push([Date.parse(ticket_outflow[i][0]),ticket_outflow[i][category_index]]);
-    			
-    			
-    		//	dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
-    		}
-        	
-        	
-        Highcharts.chart(e, {
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-             },
-            
+		incident_date = parseISOLocal(line[i][0])
+		incident_data.push([ Date.parse(line[i][0]), line[i][1] ]);
 
-             chart: {
-                 zoomType: 'x'
-             },
-             title: {
-                 text:  title
-             },
-             subtitle: {
-                 text: document.ontouchstart === undefined ?
-                         'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-             },
-             xAxis: {
-            	 type: 'datetime',
-            	    labels: {
-            	      format: '{value:%Y-%b-%e}'
-            	    }},
-             yAxis: {
-                 title: {
-                     text: 'Inflow Count'
-                 }
-             },
-             legend: {
-                 enabled: false
-             }, 
-                    	 
-            series: [{
-                    name: 'Incident',
-                    data: inflow_data
-            },
-            {
-                name: 'Incident',
-                data: outflow_data
-        }
-               ]
-            
-            })};
-        
+		// dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
+	}
+	var sr_data = []
+	for ( var i in line) {
+
+		sr_date = parseISOLocal(line[i][0])
+		sr_data.push([ Date.parse(line[i][0]), line[i][2] ]);
+
+		// dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
+	}
+
+	Highcharts
+			.chart(
+					e,
+					{
+						chart : {
+							plotBackgroundColor : null,
+							plotBorderWidth : null,
+							plotShadow : false,
+						},
+
+						chart : {
+							zoomType : 'x'
+						},credits : {
+							enabled : false
+						},
+						exporting : {
+							enabled : false
+						},
+						title : {
+							text : title
+						},
+						subtitle : {
+							text : document.ontouchstart === undefined ? 'Click and drag in the plot area to zoom in'
+									: 'Pinch the chart to zoom in'
+						},
+						xAxis : {
+							type : 'datetime',
+							labels : {
+								format : '{value:%Y-%b-%e}'
+							}
+						},
+						yAxis : {
+							title : {
+								text : 'Inflow Count'
+							}
+						},
+						legend : {
+							enabled : false
+						},
+						// var object={'positive':94,'neutral':2,'negative':2};
+
+						series : [ {
+							name : "Incident",
+							data : incident_data
+						}, {
+							name : "SR",
+							data : sr_data
+						} ]
+
+					})
+};
+
+function InflowVSOutflow(ticket_inflow, ticket_outflow, title, category, e) {
+
+	if (category == 'Incident') {
+		category_index = 1
+	} else {
+		category_index = 2
+	}
+	/*
+	 * var inflow_data=[]; for(var i in ticket_inflow){
+	 * 
+	 * inflow_date=parseISOLocal(ticket_inflow[i][0]) inflow_data.push({"name":
+	 * inflow_date,"y":ticket_inflow[i][category_index]});
+	 * 
+	 *  // dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]}); }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */var inflow_data = [];
+	for ( var i in ticket_inflow) {
+
+		inflow_data.push([ Date.parse(ticket_inflow[i][0]),
+				ticket_inflow[i][category_index] ]);
+
+		// dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
+	}
+
+	var outflow_data = []
+	for ( var i in ticket_outflow) {
+
+		// outflow_date=parseISOLocal(ticket_outflow[i][0])
+		outflow_data.push([ Date.parse(ticket_outflow[i][0]),
+				ticket_outflow[i][category_index] ]);
+
+		// dat.push({"name":Date.parse(line[i][0]),"y":line[i][1]});
+	}
+
+	Highcharts
+			.chart(
+					e,
+					{
+						chart : {
+							plotBackgroundColor : null,
+							plotBorderWidth : null,
+							plotShadow : false,
+						},
+
+						chart : {
+							zoomType : 'x'
+						},
+						credits : {
+							enabled : false
+						},
+						exporting : {
+							enabled : false
+						},
+						title : {
+							text : title
+						},
+						subtitle : {
+							text : document.ontouchstart === undefined ? 'Click and drag in the plot area to zoom in'
+									: 'Pinch the chart to zoom in'
+						},
+						xAxis : {
+							type : 'datetime',
+							labels : {
+								format : '{value:%Y-%b-%e}'
+							}
+						},
+						yAxis : {
+							title : {
+								text : 'Inflow Count'
+							}
+						},
+						legend : {
+							enabled : false
+						},
+
+						series : [ {
+							name : 'Incident',
+							data : inflow_data
+						}, {
+							name : 'Incident',
+							data : outflow_data
+						} ]
+
+					})
+};
--- PED/static/index.js
+++ PED/static/index.js
@@ -60,42 +60,28 @@ function doSomething() {
 	})
 }
 
-
-
-/* 
-$(function fillData() {
-    $('#table').bootstrapTable({
-        data: data
-    });
+/*
+ * $(function fillData() { $('#table').bootstrapTable({ data: data });
  */
-	
-/*	
-function fillData() {
+
+/*
+ * function fillData() { console.log("asdfasdf");
+ * 
+ * $.ajax({ type : "POST", url : "/pull_data/", data : { id_select_Sprint :
+ * $('#id_select_Sprint').val(), pulldata : 'Yes' },
+ * 
+ * success : function(data) { $('#table').bootstrapTable({ data: data } }) } }
+ */
+
+
+ 
+
+function getLatestData() {
 	console.log("asdfasdf");
 
 	$.ajax({
 		type : "POST",
-		url : "/pull_data/",
-		data : {
-			id_select_Sprint : $('#id_select_Sprint').val(),
-			pulldata : 'Yes'
-		},
-
-		success : function(data) {
-		      $('#table').bootstrapTable({
-        data: data
-
-		}
-	})
-}
-}*/
-
-function fillData() {
-	console.log("asdfasdf");
-
-	$.ajax({
-		type : "POST",
-		url : "/pull_data/",
+		url : "/pullLatestData/",
 		data : {
 			id_select_Sprint : $('#id_select_Sprint').val(),
 			pulldata : 'Yes'
@@ -103,63 +89,270 @@ function fillData() {
 
 		success : function(data) {
 			console.log("asdfasdf");
-
-			$('#table').bootstrapTable({
-		          data: [data] })
- 			 
+			disableAllElements()
+			$('#Sync_Job_Status').html(data.sync_job_status);
+ 
 		}
 	})
 }
+
+function disableAllElements() {
+	document.getElementById("container1").style.display = 'none';
+	document.getElementById("container2").style.display = 'none';
+	document.getElementById("container3").style.display = 'none';
+	document.getElementById("container4").style.display = 'none';
+	document.getElementById("container5").style.display = 'none';
+	document.getElementById("container6").style.display = 'none';
+	document.getElementById("pie_container1").style.display = 'none';
+	document.getElementById("pie_container2").style.display = 'none';
+	document.getElementById("pie_container3").style.display = 'none';
+	document.getElementById("pie_container4").style.display = 'none';
+	document.getElementById("pie_container5").style.display = 'none';
+	document.getElementById("pie_container6").style.display = 'none';
+	document.getElementById("table-container").style.display = 'none';
+}
+
+
+ 
+
+
+function getTicketData() {
+	console.log("asdfasdf");
+	disableAllElements();
+	document.getElementById("table-container").style.display = 'block';
+
+	$.ajax({
+		type : "POST",
+		url : "/getTicketData/",
+		"dataSrc" : "",
+		data : {
+			id_select_Sprint : $('#id_select_Sprint').val(),
+			getTicketData : 'Yes'
+		},
+
+		success : function(data) {
+
+			/*
+			 * $('#ticket_table').bootstrapTable({ data:
+			 * [data.daily_ticket_inflow] })
+			 */
+
+			$(document).ready(function() {
+				
+			var	oTable=	$('#ticket_table').DataTable({
+					data : data.daily_ticket_inflow,
+					 paging: true,
+					    searching: true,
+					    destroy: true,
+					   /* $(".dataTables_length select").addClass("selectpicker sel_id"),*/
+
+
+					columns : [ {
+						title : "TICKET"
+					}, {
+						title : "CATEGORY"
+					}, {
+						title : "DISPOSITION"
+					}, {
+						title : "CONTROL_NUMBE"
+					}, {
+						title : "BUG_NUMBER"
+					}, {
+						title : "STATUS"
+					}, {
+						title : "CREATED"
+					}, {
+						title : "ASSIGNED"
+					} ]
+				});
+				
+			onhover()
+				  oTable.$('tr').tooltip( {
+				        "delay": 0,
+				        "track": true,
+				        "fade": 250
+				    } );
+				
+			});
+
+		}
+	})
+}
+
+
+
+
+function onhover(){
+	
+	$("#ticket_table  tbody tr").on('mouseover',function () {
+		var thText = $(this).text();
+		var hoverTxt;
+		$.ajax({
+		url : "/sprint_summary_post_form",
+		async : false,
+		success : function(respText) {
+		hoverTxt = respText;
+		}
+		});
+		this.setAttribute( 'title', hoverTxt.script );
+		});
+	/*
+$('#ticket_table tbody tr').each( function() {	
+    var sTitle;
+    var nTds = $('td', this);
+    var ticket_num = $(nTds[0]).text();
+
+    $.ajax({
+		type : "POST",
+		url : "/sprint_summary_post_form/",
+	    data: "ASDF",
+
+		success : function(data) {
+			console.log(data)
+        sTitle = data.script;
+		}
+    
+              } )
+              this.setAttribute( 'title', data.script );
+*/	
+	
+    
+        
+    
+	  }
+
 
 
 
 function pie_data() {
- 
-    document.getElementById("container1").style.display='block';
-    document.getElementById("container2").style.display='block';
-    document.getElementById("container3").style.display='none';
-    document.getElementById("container4").style.display='none';
-    document.getElementById("container5").style.display='none';
-    document.getElementById("container6").style.display='none';
 
-    
-    
-    $.ajax({
-		type : "POST",
-		url : "/pie_data/",
-		data : {
-			id_select_Sprint : $('#id_select_Sprint').val(),
-			pie_data : 'Yes'
-		},
+	disableAllElements()
 
-		success : function(data) {
-			
-			console.log(data["pie_dat"])
-			console.log(data.pie_dat['Incident'])
- 			console.log(data["pie_dat"]['Service Request'])
-			console.log(data["pie_dat"]['Incident'])
-			pie=data.pie_dat;
-			var  dat=[];
-			for(i in pie ){
-				dat.push({"name":i,"y":pie[i]});
-			}
-			draw_pie(dat,'container1');
-			draw_pie(dat,'container2');
-		}
-	})
+	/*
+	 * document.getElementById("container1").style.display = 'none';
+	 * document.getElementById("container2").style.display = 'none';
+	 * document.getElementById("container3").style.display = 'none';
+	 * document.getElementById("container4").style.display = 'none';
+	 * document.getElementById("container5").style.display = 'none';
+	 * document.getElementById("container6").style.display = 'none';
+	 */
+	document.getElementById("pie_container1").style.display = 'block';
+	document.getElementById("pie_container2").style.display = 'block';
+	document.getElementById("pie_container3").style.display = 'block';
+	document.getElementById("pie_container4").style.display = 'block';
+	document.getElementById("pie_container5").style.display = 'block';
+	document.getElementById("pie_container6").style.display = 'block';
+
+	$
+			.ajax({
+				type : "POST",
+				url : "/pie_data/",
+				data : {
+					id_select_Sprint : $('#id_select_Sprint').val(),
+					pie_data : 'Yes'
+				},
+
+				success : function(data) {
+
+					/*
+					 * console.log(data["pie_dat"])
+					 * console.log(data.pie_dat['Incident'])
+					 * console.log(data["pie_dat"]['Service Request'])
+					 * console.log(data["pie_dat"]['Incident'])
+					 */
+
+					incident_sr_count = data.incident_sr_count;
+					ticket_status = data.ticket_status;
+					ticket_source = data.ticket_source;
+					open_tickets_by_queue = data.open_tickets_by_queue;
+					open_sr_ticket_source = data.open_sr_ticket_source;
+					open_incident_ticket_source = data.open_incident_ticket_source;
+
+					var isc = [];
+					for (i in incident_sr_count) {
+						isc.push({
+							"name" : i,
+							"y" : incident_sr_count[i]
+						});
+					}
+
+					var tstatus = [];
+					for (i in ticket_status) {
+						tstatus.push({
+							"name" : i,
+							"y" : ticket_status[i]
+						});
+					}
+
+					var ts = [];
+					for (i in ticket_source) {
+						ts.push({
+							"name" : i,
+							"y" : ticket_source[i]
+						});
+					}
+
+					var otq = [];
+					for (i in open_tickets_by_queue) {
+						otq.push({
+							"name" : i,
+							"y" : open_tickets_by_queue[i]
+						});
+					}
+
+					var oss = [];
+					for (i in open_sr_ticket_source) {
+						oss.push({
+							"name" : i,
+							"y" : open_sr_ticket_source[i]
+						});
+					}
+
+					var ois = [];
+					for (i in open_incident_ticket_source) {
+						ois.push({
+							"name" : i,
+							"y" : open_incident_ticket_source[i]
+						});
+					}
+
+					draw_pie(isc, 'pie_container1', "Incident vs SR Count");
+					draw_pie(tstatus, 'pie_container2',
+							"Current Open Tickets By Status");
+					draw_pie(ts, 'pie_container3',
+							'Current Open Tickets By Source');
+					draw_pie(otq, 'pie_container4',
+							'Current Open Tickets By Queue');
+
+					draw_pie(oss, 'pie_container5', 'Current Open SR By Source');
+					draw_pie(ois, 'pie_container6',
+							'Current Open Incidents By Source');
+					/*
+					 * draw_pie(dat,'pie_container7');
+					 * draw_pie(dat,'pie_container8');
+					 * 
+					 * 
+					 * draw_pie(dat,'pie_container9');
+					 * draw_pie(dat,'pie_container10');
+					 * draw_pie(dat,'pie_container11');
+					 * draw_pie(dat,'pie_container12');
+					 */
+				}
+			})
 }
 
-
 function Draw_InflowVSOutflow() {
-	 
-    document.getElementById("container1").style.display='none';
-    document.getElementById("container2").style.display='none';
-    document.getElementById("container3").style.display='block';
-    document.getElementById("container4").style.display='block';
-    document.getElementById("container5").style.display='none';
-    document.getElementById("container6").style.display='none';
-    
-    
+
+	disableAllElements();
+	
+	document.getElementById("container1").style.display = 'block';
+	document.getElementById("container2").style.display = 'block';
+	document.getElementById("container3").style.display = 'block';
+	document.getElementById("container4").style.display = 'block';
+	/*document.getElementById("container5").style.display = 'none';
+	document.getElementById("container6").style.display = 'none';
+*/
+ 
 	$.ajax({
 		type : "POST",
 		url : "/InflowVSOutflow/",
@@ -169,82 +362,106 @@ function Draw_InflowVSOutflow() {
 		},
 
 		success : function(data) {
-			 console.log('asdf')
-			 
-			InflowVSOutflow(data.daily_ticket_inflow,data.daily_ticket_outflow,'Daily '+'Incident'+' Inflow vs Outflow','Incident','container3');
+			console.log('asdf')
 
-			 InflowVSOutflow(data.daily_ticket_inflow,data.daily_ticket_outflow,'Daily '+'SR'+' Inflow vs Outflow','SR','container4');
+			InflowVSOutflow(data.daily_ticket_inflow,
+					data.daily_ticket_outflow, 'Daily ' + 'Incident '
+							+ 'Inflow vs Outflow', 'Incident', 'container1');
+
+			InflowVSOutflow(data.daily_ticket_inflow,
+					data.daily_ticket_outflow, 'Daily ' + 'SR '
+							+ 'Inflow vs Outflow', 'SR', 'container2');
+
+			InflowVSOutflow(data.weekly_ticket_inflow,
+					data.weekly_ticket_outflow, 'Weekly ' + 'Incident '
+							+ 'Inflow vs Outflow', 'Incident', 'container3');
+
+			InflowVSOutflow(data.weekly_ticket_inflow,
+					data.weekly_ticket_outflow, 'Weekly ' + 'SR '
+							+ 'Inflow vs Outflow', 'SR', 'container4');
+
 		}
 	})
 }
+function Disable_elements() {
 
+	document.getElementById("pie_container1").style.display = 'none';
+	document.getElementById("pie_container2").style.display = 'none';
+	document.getElementById("pie_container3").style.display = 'none';
+	document.getElementById("pie_container4").style.display = 'none';
+	document.getElementById("pie_container5").style.display = 'none';
+	document.getElementById("pie_container6").style.display = 'none';
+
+}
 function Draw_Incident_VS_SR() {
-	 
-    document.getElementById("container1").style.display='none';
-    document.getElementById("container2").style.display='none';
-    document.getElementById("container3").style.display='block';
-    document.getElementById("container4").style.display='block';
-    document.getElementById("container5").style.display='block';
-    document.getElementById("container6").style.display='block';
-    
-    
+
+	
+	disableAllElements();
+	/*document.getElementById("container1").style.display = 'none';
+	document.getElementById("container2").style.display = 'none';
+	*/document.getElementById("container3").style.display = 'block';
+	document.getElementById("container4").style.display = 'block';
+	document.getElementById("container5").style.display = 'block';
+	document.getElementById("container6").style.display = 'block';
+
+	Disable_elements();
+
 	$.ajax({
 		type : "POST",
 		url : "/Incident_VS_SR/",
 		data : {
 			id_select_Sprint : $('#id_select_Sprint').val(),
-			Incident_VS_SR: 'Yes'
+			Incident_VS_SR : 'Yes'
 		},
 
 		success : function(data) {
-		 
-			Incident_VS_SR(data.daily_ticket_inflow,'Daily Ticket Inflow','container3');
-			Incident_VS_SR(data.daily_ticket_outflow,'Daily Ticket Outflow','container4');
-			Incident_VS_SR(data.weekly_ticket_inflow,'Weekly Ticket Inflow','container5');
-			Incident_VS_SR(data.weekly_ticket_outflow,'Weekly Ticket Outflow','container6');
-		
+
+			Incident_VS_SR(data.daily_ticket_inflow, 'Daily Ticket Inflow',
+					'container3');
+			Incident_VS_SR(data.daily_ticket_outflow, 'Daily Ticket Outflow',
+					'container4');
+			Incident_VS_SR(data.weekly_ticket_inflow, 'Weekly Ticket Inflow',
+					'container5');
+			Incident_VS_SR(data.weekly_ticket_outflow, 'Weekly Ticket Outflow',
+					'container6');
+
 		}
 	})
 }
 
 function parseISOLocal(s) {
-	  var b = s.split('-');
-	  //datestring=String(parseInt(b[0])+1) + b[1] +b[2]
-	  datestring=b[0] + b[1] +b[2]
-	  return new Date(datestring);
-	}
+	var b = s.split('-');
+	// datestring=String(parseInt(b[0])+1) + b[1] +b[2]
+	datestring = b[0] + b[1] + b[2]
+	return new Date(datestring);
+}
 
+function bokeh_pie() {
 
-function bokeh_pie(){ 
+	document.getElementById('bokeh_ch1').innerHTML = "";
+	document.getElementById('bokeh_ch2').innerHTML = "";
 
-	document.getElementById('bokeh_ch1').innerHTML=""; 
-	document.getElementById('bokeh_ch2').innerHTML=""; 
+	var plt = Bokeh.Plotting;
 
-	var plt =  Bokeh.Plotting;
- 
-	
-	var inc_sr={labels:['asdf','Sr'],
-		values: [53,65]
-		
+	var inc_sr = {
+		labels : [ 'asdf', 'Sr' ],
+		values : [ 53, 65 ]
+
 	};
-	
-	
-	 var p0 = Bokeh.Charts.pie(inc_sr,{palette:['#FF8C00'	,'#A0522D'],
-        inner_radius: 0.0,
-        start_angle: Math.PI / 2,
-        end_angle: 5 * Math.PI / 2
-    });
-    
-	 p0.document.getElementById('bk-root')
-	 
-	 
-    document.getElementById('bokeh_ch1').innerHTML=plt.show(p0);
-    document.getElementById('bokeh_ch2').innerHTML=plt.show(p0);
-	
-	
-	
-	}
 
+	var p0 = Bokeh.Charts.pie(inc_sr, {
+		palette : [ '#FF8C00', '#A0522D' ],
+		inner_radius : 0.0,
+		start_angle : Math.PI / 2,
+		end_angle : 5 * Math.PI / 2
+	});
+
+	p0.document.getElementById('bk-root')
+
+	document.getElementById('bokeh_ch1').innerHTML = plt.show(p0);
+	document.getElementById('bokeh_ch2').innerHTML = plt.show(p0);
+
+}
 
 function doPOST() {
 	console.log("asdfasdf");
@@ -276,30 +493,26 @@ function doPOSTForm(e) {
 
 		success : function(data) {
 
-			//$(div).html(data["div"]);
-			//$("head").append(data["script"]);
- 
-			
-			//var bokeh_data = JSON.parse(data);
-		      $('#bokeh_graph1').html(data.div1);
-		      $('#bokeh_graph2').html(data.div2);
-		      $('#bokeh_graph3').html(data.div2);
-		      $("head").append(data.script);
-		      $("head").append(data.script);
-		      $("head").append(data.script);
+			// $(div).html(data["div"]);
+			// $("head").append(data["script"]);
 
-			
-			
-/*			var x= data["div"];
-			var y = data["script"];
-			
-			 var html = $.parseHTML( data );
+			// var bokeh_data = JSON.parse(data);
+			$('#bokeh_graph1').html(data.div1);
+			$('#bokeh_graph2').html(data.div2);
+			$('#bokeh_graph3').html(data.div2);
+			$("head").append(data.script);
+			$("head").append(data.script);
+			$("head").append(data.script);
 
-			$("#bokeh_id").html(eval(html['bokeh_id']) );
-			console.trace()
-			console.log("this is console");
-*/			
-			 
+			/*
+			 * var x= data["div"]; var y = data["script"];
+			 * 
+			 * var html = $.parseHTML( data );
+			 * 
+			 * $("#bokeh_id").html(eval(html['bokeh_id']) ); console.trace()
+			 * console.log("this is console");
+			 */
+
 		}
 	})
 }
